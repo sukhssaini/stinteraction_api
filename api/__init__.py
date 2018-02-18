75f64159e65f273eba3f9d032df0f8c5994dfb5e
sukhssaini <sainisukha88@gmail.com>
2018-02-18 15:34:21 +0530

    added env variables support

--- api/__init__.py
+++ api/__init__.py
@@ -1,12 +1,13 @@
 from flask import Flask
 from flask_pymongo import PyMongo
 from flask_restful import Api
+import os
 
 app = Flask(__name__)
 api = Api(app)
 
-app.config['MONGO_DBNAME'] = 'test1'
-app.config['MONGO_URI'] = 'mongodb://localhost:27017/test1'
+app.config['MONGO_DBNAME'] = os.environ.get("DATABASE_NAME")
+app.config['MONGO_URI'] = os.environ.get("MONGO_CONNECTION_URI")
 
 mongo = PyMongo(app)
 
--- run.py
+++ run.py
@@ -1,3 +1,9 @@
+from os.path import join, dirname
+from os import environ
+from dotenv import Dotenv
 from api import app
+
+environment = Dotenv(join(dirname(__file__), ".env"))
+environ.update(environment)
 
 app.run(debug=True, port=5002)
